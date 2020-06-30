f1ee92a2d6a25ffff2da226bf39cd3e3a4c61122
Joao Gabriel Zo Lanes <joao.lanes@ppm-spo-9041.localdomain>
2020-06-30 17:15:57 -0300

    ops

--- day-one/RocketEquation.hs
+++ day-one/RocketEquation.hs
@@ -6,8 +6,22 @@ fuelRequired x =  (floor $ x/3) - 2
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile
 
-main :: IO Integer
-main = do
+pt1main :: IO Integer
+pt1main = do
   lines <- readLines "/Users/joao.lanes/dev/Haskell/AoC/day-one/fuel.txt"
   let xs = fmap (\x -> read x :: Double) lines
   return (sum $ map fuelRequired xs)
+
+recFuelRequired :: [Double] -> Integer -> Integer
+recFuelRequired [] acc = acc
+recFuelRequired (x:xs) acc
+  | fuel > 0 = recFuelRequired ((fromIntegral fuel):xs) (acc + fuel)
+  | otherwise = recFuelRequired xs acc
+  where
+    fuel = fuelRequired x
+
+pt2main :: IO Integer
+pt2main = do
+  lines <- readLines "/Users/joao.lanes/dev/Haskell/AoC/day-one/fuel.txt"
+  let totalFuel = recFuelRequired (map (\x -> read x :: Double) lines) 0
+  return totalFuel
